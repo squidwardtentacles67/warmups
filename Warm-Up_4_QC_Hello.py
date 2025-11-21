5b5f74aec95a584ceb157a049cd353db299ee1b9
squidwardtentacles67 <zachary122649@mcpsva.org>
2025-11-21 14:41:12 -0500

    Update Warm-Up_4_QC_Hello.py

--- Warm-Up_4_QC_Hello.py
+++ Warm-Up_4_QC_Hello.py
@@ -5,6 +5,8 @@ Created: <MM/DD/YYYY>
 Instructor: Holtslander
 """
 
+import time
+
 def hello():
     """
     Asks the user to input their name. Stores their name in a variable and prints the message:
@@ -13,7 +15,19 @@ def hello():
     """
     ### YOUR CODE GOES HERE ###
 
+    while True:
+        name = input("What is your name?\n")
+        time.sleep(1)
+        
+        if name == "" or name == " "*len(name):
+            print("Please input at least one character!")
+            time.sleep(3)
+        else:
+            print(f"Hello, {name}")
+            break
 
 ### YOU SHOULD NOT NEED TO CHANGE ANYTHING HERE ###
 if __name__ == '__main__':
+
+    #  hello bro 👀
     hello()
